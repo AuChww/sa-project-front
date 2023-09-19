137144ab4921bc5a8e9245713fd55f864c2e7fd1
Chanawut <chanawut.w@ku.th>
2023-09-19 13:59:30 +0700

    au-navbar

---
+++ components/MenuLink.vue
@@ -0,0 +1,12 @@
+<template>
+    <NuxtLink class="inline-block px-4 bg-blue-300 hover:bg-blue-400"
+    :to="to">
+    <slot />
+    </NuxtLink>
+</template>
+
+<script setup lang="ts">
+const props = defineProps([
+    'to'
+])
+</script>
---
+++ components/NavBar.vue
@@ -0,0 +1,11 @@
+
+import { NuxtLink } from '#build/components';
+<template>
+  <nav>
+    <MenuLink to="/">Home</MenuLink>
+    <MenuLink to="/customer/shopping-cart">Shop Cart</MenuLink>
+    <MenuLink to="/customer/order-credit">Profile</MenuLink>
+    <MenuLink to="/customer/profile">Profile</MenuLink>
+    <MenuLink to="/login/index">Log in</MenuLink>
+  </nav>
+</template>
