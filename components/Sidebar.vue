4d676b41434926029499a54cb716f397d7281f5d
chanawut <chanawut.w@ku.th>
2023-10-06 12:12:39 +0700

    dropdown

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="sidebar" :class="{ 'sidebar-open': isOpen }">
+    <div class="sidebar z-50" :class="{ 'sidebar-open': isOpen }">
       <div class="h-full px-12 py-4 overflow-y-auto bg-gray-50 dark:bg-blue-800">
           <ul class="space-y-2 font-medium">
               <li>
@@ -48,7 +48,7 @@
           </ul>
       </div>
     </div>
-    <aside class="fixed top-80 left-1 z-40 w-30 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+    <aside class="fixed top-80 left-1 z-40 w-30 h-screen transition-transform -translate-x-full sm:translate-x-0 z-50" aria-label="Sidebar">
       <button class="bg-blue-800 rounded-lg hover:bg-green-100 dark:hover:bg-blue-400 group" @click="toggleSidebar">
         <svg class="flex-shrink-0 w-10 h-10 text-blue-500 dark:text-blue-300 dark:group-hover:text-blue-900 hover:scale-125 transition-all duration-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
             <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"/>
